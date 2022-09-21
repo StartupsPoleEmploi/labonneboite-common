33d68fa4c8c0c13a43247aa94893d9c286a8d092
sylvain.touret <sylvain.touret@delia.tech>
2022-09-21 16:18:42 +0200

    typo in script path

--- scripts/python_quality.sh
+++ scripts/python_quality.sh
@@ -6,15 +6,15 @@ poetry install
 mkdir testResults
 
 # quality
-flake8 --output-file testResults/flake8.txt labonneboite_common 
-flake8_junit testResults/flake8.txt testResults/flake8.xml
+poetry run flake8 --output-file testResults/flake8.txt labonneboite_common 
+poetry run flake8_junit testResults/flake8.txt testResults/flake8.xml
 rm testResults/flake8.txt
 
 # unit test
-pytest --verbose --junitxml=testResults/pytest.xml 
+poetry run pytest --verbose --junitxml=testResults/pytest.xml 
 
 # coverage
-coverage run -m pytest --verbose --junitxml=testResults/coverage.xml
+poetry run coverage run -m pytest --verbose --junitxml=testResults/coverage.xml
 
 # build the package
 poetry build
