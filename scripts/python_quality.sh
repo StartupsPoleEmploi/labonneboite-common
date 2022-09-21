bf94d6ea27b32c59c40af5c33396b627174a9b42
sylvain.touret <sylvain.touret@delia.tech>
2022-09-21 15:29:32 +0200

    update folder name for output results

--- scripts/python_quality.sh
+++ scripts/python_quality.sh
@@ -1,17 +1,17 @@
 # /bin/bash
 set -e
 
-mkdir tests
+mkdir testResults
 
 # quality
-flake8 --output-file tests/flake8.txt labonneboite_common 
-flake8_junit tests/flake8.txt tests/flake8.xml
-rm tests/flake8.txt
+flake8 --output-file testResults/flake8.txt labonneboite_common 
+flake8_junit testResults/flake8.txt testResults/flake8.xml
+rm testResults/flake8.txt
 
-# unit tests
-pytest --verbose --junitxml=tests/pytest.xml 
+# unit test
+pytest --verbose --junitxml=testResults/pytest.xml 
 
 # coverage
-coverage run -m pytest --verbose --junitxml=tests/coverage.xml
+coverage run -m pytest --verbose --junitxml=testResults/coverage.xml
 
 
