adf968c0d66180630244e5689622db122f14c969
sylvain.touret <sylvain.touret@delia.tech>
2022-09-21 15:11:17 +0200

    update to flake8

--- .ci/python-tests.sh
+++ .ci/python-tests.sh
@@ -1,11 +1,16 @@
 # /bin/bash
 set -e
 
+mkdir tests
+
 # quality
-flake8 labonneboite_common/ --exclude tests || echo 'FAILED LINT'
-
+flake8 --output-file tests/flake8.txt labonneboite_common 
+flake8_junit tests/flake8.txt tests/flake8.xml
+rm tests/flake8.txt
 # unit tests
-pytest --verbose --junitxml=pytest.xml  || echo 'FAILED UNIT TESTS'
+pytest --verbose --junitxml=tests/pytest.xml 
 
-coverage run -m pytest --verbose --junitxml=pytest.xml  || echo 'FAILED UNIT TESTS'
+# coverage
+coverage run -m pytest --verbose --junitxml=tests/coverage.xml
+
 
