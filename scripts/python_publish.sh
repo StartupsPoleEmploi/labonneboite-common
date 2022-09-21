4b0780638fe0dce0a83b939943d872071835fc59
sylvain.touret <sylvain.touret@delia.tech>
2022-09-21 17:26:10 +0200

    update to publish method

--- scripts/python_publish.sh
+++ scripts/python_publish.sh
@@ -1,6 +1,9 @@
 # /bin/bash
 set -e
 
+# apply the version request by user
+poetry version $CI_COMMIT_TAG
+
 # build the package
 poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
