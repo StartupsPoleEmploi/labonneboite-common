76d59c55a80c4e3e5fa7a877b93c449d1b287c5b
sylvain.touret <sylvain.touret@delia.tech>
2022-09-21 16:15:45 +0200

    added poetry install and no lock

---
+++ scripts/python_install.sh
@@ -0,0 +1,4 @@
+# /bin/bash
+set -e
+pip install poetry
+poetry install --no-root
--- scripts/python_publish.sh
+++ scripts/python_publish.sh
@@ -1,7 +1,7 @@
 # /bin/bash
 set -e
 
-poetry install
+
 
 # build the package
 poetry publish --build --username $PYPI_USERNAME --password $PYPI_PASSWORD
