15b146e3ce177a42be44953194c859ae059e8e3e
sylvain.touret <sylvain.touret@delia.tech>
2022-09-22 10:36:13 +0200

    removed devops jobs to global devops repo

--- scripts/python_publish.sh
+++
@@ -1,9 +0,0 @@
-# /bin/bash
-set -e
-
-# apply the version request by user
-poetry version $CI_COMMIT_TAG
-
-# build the package
-poetry publish --build --username __token__ --password $PYPI_TOKEN
-
