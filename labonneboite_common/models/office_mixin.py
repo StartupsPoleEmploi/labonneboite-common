81a4260b255839955a9c0bde243607bcbc484148
Sylvain Touret <sylvain.touret@delia.tech>
2022-10-14 14:21:56 +0000

    linting fix

--- labonneboite_common/models/office_mixin.py
+++ labonneboite_common/models/office_mixin.py
@@ -15,7 +15,7 @@ class PrimitiveOfficeMixin(object):
     each time you add/change/remove a field here to keep all models
     in sync.
     """
-    __table_args__ =  (
+    __table_args__ = (
         {
             'mysql_default_charset': 'utf8mb4',
             'mysql_collate': 'utf8mb4_unicode_ci'
